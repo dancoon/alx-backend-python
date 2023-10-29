117b87cdbd79942131e4afca2f3c7f534fc6408e
dancoon <dancun.ian@students.jkuat.ac.ke>
2023-10-29 18:22:54 +0300

    Parameterize and patch as decorators

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -15,8 +15,8 @@ class TestGithubOrgClient(unittest.TestCase):
     Test the GithubOrgClient class methods
     """
     @parameterized.expand([
-        ("google"),
-        ("abc")
+        ("google",),
+        ("abc",)
     ])
     @patch("client.get_json", return_value={"payload": True})
     def test_org(self, org, mocked):
